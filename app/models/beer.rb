198fa66147dc7a20d0df813ebfcf771adff96f62
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2018-09-13 19:49:48 +0300

    Viikon 2 tehtävät 3-6

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,4 +1,16 @@
 class Beer < ApplicationRecord
     belongs_to :brewery
     has_many :ratings
+
+    # laske oluen ratingien keskiarvon
+    def average_rating
+        sum = 0
+        ratings = Rating.where beer_id: self.id
+        if ratings.empty?
+            return sum
+        end
+        sum = ratings.reduce(0) {|t, rating| t + rating.score}
+        return sum*1.0/ratings.size
+    end
+
 end
--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,10 @@
 class Rating < ApplicationRecord
     belongs_to :beer
+
+    # olion parempi merkkijonoesitys
+    def to_s
+        beer = Beer.find(beer_id)
+        "#{beer.name} #{score}"
+    end
+
 end
