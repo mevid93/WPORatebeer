d00c3611676e456370e5bbc3ec9044be012b9e49
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2018-09-24 18:30:04 +0300

    Viikon 4 tehtävä 1

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,10 +15,10 @@ class User < ApplicationRecord
   # testi tarkastaa, että salasana on vähintään 4 merkkiä pitkä,
   # sisältää yhden ison kirjaimen A-Z ja vähintään yhden numeron
   def password_validation
-    errors.add(:password, "must be at least 4 characters long") if password.length < 4
+    errors.add(:password, "must be at least 4 characters long") if password.nil? || password.length < 4
 
-    errors.add(:password, "must contain at least 1 capital letter") if password !~ /\A([a-zA-Z]|\d)*[A-Z]([a-zA-Z]|\d)*\z/
+    errors.add(:password, "must contain at least 1 capital letter") if password.nil? || password !~ /\A([a-zA-Z]|\d)*[A-Z]([a-zA-Z]|\d)*\z/
 
-    errors.add(:password, "must contain at least 1 digit") if password !~ /\A([a-zA-Z]|\d)*[\d]([a-zA-Z]|\d)*\z/
+    errors.add(:password, "must contain at least 1 digit") if password.nil? || password !~ /\A([a-zA-Z]|\d)*[\d]([a-zA-Z]|\d)*\z/
   end
 end
