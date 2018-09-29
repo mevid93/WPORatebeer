3c5fff363836aa3056a194354aa016d58f632f5a
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2018-09-29 16:17:35 +0300

    Viikon 4 tehtävät 8-9

--- spec/features/users_page_spec.rb
+++ spec/features/users_page_spec.rb
@@ -38,10 +38,10 @@ describe "Users page" do
   before :each do
     @user = FactoryBot.create(:user)
     @user2 = FactoryBot.create(:user, username: "Toinen", password: "Toinen1", password_confirmation:"Toinen1")
-    @beer = FactoryBot.create(:beer, name: "Testiolut")
+    @beer = FactoryBot.create(:beer, name: "Testiolut", style: "Testityyli")
     @beer2 = FactoryBot.create(:beer, name: "NoNoNoBeer")
     set_many_ratings_for_beer({user: @user, beer: @beer}, 23, 2)
-    set_many_ratings_for_beer({user: @user2, beer: @beer2}, 34, 42)
+    set_many_ratings_for_beer({user: @user2, beer: @beer2}, 34)
     sign_in(username: "Pekka", password: "Foobar1")
   end
 
@@ -51,5 +51,19 @@ describe "Users page" do
     expect(page).to have_content("Has #{@user.ratings.count} ratings")
     expect(page).to have_content("Testiolut")
     expect(page).not_to have_content("NoNoNoBeer")
+  end
+
+  it "allows user to delete his own raiting" do
+    visit user_path(@user)
+    rating_id = @user.ratings.first.id  # poistettavan ratingin id
+    expect{
+      find("a[href='/ratings/#{rating_id}']").click
+    }.to change{@user.ratings.count}.by(-1)
+  end
+
+  it "lists users favorite beer style and brewery when user has ratings" do
+    visit user_path(@user)
+    expect(page).to have_content("Favorite beer style: #{@beer.style}")
+    expect(page).to have_content("Favorite brewery: #{@beer.brewery.name}")
   end
 end
