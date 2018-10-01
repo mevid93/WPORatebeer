5f795be18d5629cec77deeecc155f6b6bcf4c3b9
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2018-10-01 18:13:42 +0300

    Viikon 5 tehtävä 1

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,9 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   get 'signin', to: 'sessions#new'
+  get 'places', to: 'places#index'
+
+  post 'places', to: 'places#search'
 
   delete 'signout', to: 'sessions#destroy'
 
