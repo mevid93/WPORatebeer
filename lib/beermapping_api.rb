54688fd14b74e7cbf45f71d8a4fd2082c28c803c
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2018-10-02 12:44:07 +0300

    Viikon 5 tehtävät 2-3

---
+++ lib/beermapping_api.rb
@@ -0,0 +1,18 @@
+class BeermappingApi
+  def self.places_in(city)
+    url = "http://beermapping.com/webservice/loccity/#{key}/"
+    response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
+    places = response.parsed_response["bmp_locations"]["location"]
+    return [] if places.is_a?(Hash) && places["id"].nil?
+
+    places = [places] if places.is_a?(Hash)
+
+    places.map do |place|
+      Place.new(place)
+    end
+  end
+
+  def self.key
+    "a0ab3269a0561b40541bb658233d0acf"
+  end
+end
---
+++ spec/features/places_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe "Places" do
+  it "if one is returned by the api, it is shown at the page" do
+    allow(BeermappingApi).to receive(:places_in).with("kumpula").and_return(
+      [Place.new(name:"Oljenkorsi", id:1)]
+    )
+    visit places_path
+    fill_in('city', with:'kumpula')
+    click_button "Search"
+    expect(page).to have_content "Oljenkorsi"
+  end
+
+  it "if many are returned by the api, they all are shown at the page" do
+    allow(BeermappingApi).to receive(:places_in).with("Otaniemi").and_return(
+        [Place.new(name:"Fat Lizzard", id:1), Place.new(name:"Smökki", id:2)]
+    )
+    visit places_path
+    fill_in('city', with:"Otaniemi")
+    click_button "Search"
+    expect(page).to have_content "Fat Lizzard"
+    expect(page).to have_content "Smökki"
+  end
+
+  it "if none are returned by the api, correct message is shown at the page" do
+    allow(BeermappingApi).to receive(:places_in).with("Espoo").and_return(
+        []
+    )
+    visit places_path
+    fill_in('city', with:"Espoo")
+    click_button "Search"
+    expect(page).to have_content "No locations in Espoo"
+  end
+end
